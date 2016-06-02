19342067d1015b6c58532067675386188fbaa9be
Teagan Glenn <that@teagantotally.rocks>
2016-06-01 20:53:20 -0600

    Update connection configuration

--- src/server/services/db/knexfile.js
+++ src/server/services/db/knexfile.js
@@ -1,7 +1,14 @@
 module.exports = {
-    development: {
+    development: //'Driver=pg;Server=127.0.0.1;Port=5432;Database=magrathea;Uid=postgres;Pwd=12345;'
+    {
         client: 'pg',
-        connection: 'postgres://localhost/magrathea',
+        connection: {
+            host: '127.0.0.1',
+            port: 5432,
+            user: 'postgres',
+            password: '12345',
+            database: 'magrathea'
+        },
         migrations: {
             directory: './migrations/'
         },
