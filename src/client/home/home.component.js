eba3636a0c800d9b7c78a8ddddca4d9fb77c1158
Teagan Glenn <that@teagantotally.rocks>
2016-06-01 21:13:11 -0600

    Start trying to get http working

--- src/client/home/home.component.js
+++ src/client/home/home.component.js
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {CanActivate} from 'angular2/router';
 import {Valve} from '../valve/valve.component';
+import {Http, headers} from 'angular2/http';
 
 @Component({
     selector: 'home',
@@ -9,7 +10,16 @@ import {Valve} from '../valve/valve.component';
     template: require('./home.template.html')
 })
 class Home {
-    constructor() {}
+    constructor() {
+        this.valves = [];
+        this.http = new Http();
+        
+        this.http.get('/api/valves')
+            .map((res) => res.text())
+            .subscribe((data) => this.valves.concat(data)
+                    , (err) => console.log(err)
+                    , () => console.log('Complete'));
+    }
 }
 
 
