dee1c15e78e5036f833d879e37dbfd391b2c0501
Teagan Glenn <teagan@Teagans-Air.hsd1.co.comcast.net>
2016-06-01 19:03:12 -0600

    Import routes

--- src/server/routes.js
+++ src/server/routes.js
@@ -3,6 +3,7 @@ let path = require('path');
 
 let models = {
     "login" : require(path.join(__dirname, '/models/apiLogin'))
+    , "valves": require(path.join(__dirname, '/models/apiValve'))
 }
 
 exports.models = models;
