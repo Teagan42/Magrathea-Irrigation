34f845fec97b584d3387af46e9f41a8f6016b6c9
Teagan Glenn <teagan@Teagans-Air.hsd1.co.comcast.net>
2016-06-06 20:02:20 -0600

    Update queries for getting schedules

--- src/server/services/db/daos/ScheduleDAO.js
+++ src/server/services/db/daos/ScheduleDAO.js
@@ -14,9 +14,17 @@ module.exports = {
         return Schedules().where({Id}).first();
     },
     SchedulesFor: function (date) {
+        var time = date.getHours() + ':' + date.getMinutes() + ":00";
         return Schedules()
-            .whereRaw('"StartTime" < ?', [date])
+            .whereRaw('(? >= "StartTime") AND (? <= "EndTime")', [time, time])
             .orWhere('IsRunning', true)
+            .orderBy('Priority', 'desc');
+    },
+    SchedulesToStop: function (date) {
+        var time = date.getHours() + ':' + date.getMinutes() + ":00";
+        return Schedules()
+            .whereRaw('((? < "StartTime") OR (? > "EndTime"))', [time, time])
+            .andWhere("IsRunning", true)
             .orderBy('Priority', 'desc');
     },
     RunningSchedules: function () {
