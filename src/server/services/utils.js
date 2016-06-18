58718fa3118f2be86f220e0ab92172c64b3b6c2c
Teagan Glenn <that@teagantotally.rocks>
2016-06-17 20:08:39 -0600

    Day of week converter

--- src/server/services/utils.js
+++ src/server/services/utils.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const dayOfWeek = [
+    'SUNDAY'
+    , 'MONDAY'
+    , 'TUESDAY'
+    , 'WEDNESDAY'
+    , 'THURSDAY'
+    , 'FRIDAY'
+    , 'SATURDAY'
+];
+
+function getDayOfWeek(date) {
+    if (!(date instanceof Date)) { throw new Error('Not a date'); }
+
+    return dayOfWeek[date.getDay()];
+}
+
+exports.getDayOfWeek = getDayOfWeek;
