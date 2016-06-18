f8722248ea2199ab7ca6b72eeefe5ed8317cd1cb
Teagan Glenn <that@teagantotally.rocks>
2016-06-17 20:09:02 -0600

    Helper methods

--- src/server/services/db/daos/ScheduleDAO.js
+++ src/server/services/db/daos/ScheduleDAO.js
@@ -1,9 +1,21 @@
 "use strict";
-
+var utils = require('../../utils');
 var knex = require('../knex');
 
 function Schedules() {
     return knex('Schedule');
+}
+
+function getStartQuery(date) {
+    var time = date.getHours() + ':' + date.getMinutes() + ":00";
+    var day = utils.getDayOfWeek(date);
+    return '(\'' + time + '\' >= "StartTime") AND (\'' + time + '\' <= "EndTime") AND ("DaysOfWeek" = \'ALL\' OR "DaysOfWeek" LIKE \'%' + day + '%\')';
+}
+
+function getStopQuery(date) {
+    var time = date.getHours() + ':' + date.getMinutes() + ":00";
+    var day = utils.getDayOfWeek(date);
+    return '((\'' + time + '\' < "StartTime") OR (\'' + time + '\' > "EndTime")) OR ("DaysOfWeek" NOT LIKE \'%' + day + '%\')'
 }
 
 module.exports = {
